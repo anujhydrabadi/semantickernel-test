e1aefcb7c5d3e235ea4174158c607aebf8cc9cad
Anuj Hydrabadi <anuj.hydrabadi@facets.cloud>
2024-07-29 17:07:05 +0530

    First commit

---
+++ src/main/java/test/anuj/ai/semantickernel/SemantickernelApplication.java
@@ -0,0 +1,13 @@
+package test.anuj.ai.semantickernel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemantickernelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemantickernelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/anuj/ai/semantickernel/SemantickernelApplicationTests.java
@@ -0,0 +1,13 @@
+package test.anuj.ai.semantickernel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemantickernelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
